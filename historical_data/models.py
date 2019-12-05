f625e53ffb05e41f67c72fef002ec8563ecbea61
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-12-05 15:52:59 -0800

    removed initial migration

--- historical_data/models.py
+++ historical_data/models.py
@@ -5,4 +5,12 @@ class PriceHistory(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     volume = models.DecimalField(max_digits=7, decimal_places=3)
-    
+
+
+# removing a migration
+
+# use pyhton manage.py showmigrations
+# this will produce all the migrations and give you the migrations names
+# NEXT
+# python manange.py migrate 0001_name_of_migration
+#  then we can use the show migration command to see if it worked or we could use the dbshell to check
